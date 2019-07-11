d86816d6dd5351029949055d6f2f980c3025359b
Ashley Ktorou <ashley.ktorou@justdevelop.it>
2019-07-11 10:18:39 +0100

    - Added a short description to each docbloc function  - fix the spacing on the variadic functions

--- src/BemComponent.php
+++ src/BemComponent.php
@@ -3,6 +3,11 @@ namespace PackagedUi\BemComponent;
 
 interface BemComponent
 {
-  /** @return string */
+  /**
+   * This is the root BEM class that all the other elements
+   * and modifiers will use as a base
+   *
+   * @return string
+   */
   public function getBlockName(): string;
 }
--- src/BemComponentTrait.php
+++ src/BemComponentTrait.php
@@ -3,16 +3,24 @@ namespace PackagedUi\BemComponent;
 
 trait BemComponentTrait
 {
-  /** @return string */
+  /**
+   * This is the root BEM class that all the other elements
+   * and modifiers will use as a base
+   *
+   * @return string
+   */
   abstract public function getBlockName(): string;
 
   /**
+   * Modifiers are used to dentote a modififaction of the component
+   * or specific elements inside.
+   *
    * @param string $modifier
    * @param string ...$elements
    *
    * @return string
    */
-  public function getModifier(string $modifier, string... $elements): string
+  public function getModifier(string $modifier, string ...$elements): string
   {
     if($elements)
     {
@@ -23,11 +31,14 @@ trait BemComponentTrait
   }
 
   /**
+   * Element classes are used to style specfic sections within
+   * a component while still being scoped
+   *
    * @param string ...$elements
    *
    * @return string
    */
-  public function getElementName(string... $elements): string
+  public function getElementName(string ...$elements): string
   {
     $class = $this->getBlockName();
 
