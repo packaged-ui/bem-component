1c93de82845b8925bbe72345fd5a2a737e563230
Brooke Bryan <brooke@bajb.net>
2020-08-03 09:14:42 +0100

    Stringable bem

--- src/Bem.php
+++ src/Bem.php
@@ -6,6 +6,7 @@ class Bem
   use BemComponentTrait;
 
   protected $_block;
+  protected $_classes = [];
 
   /**
    * ClassBuilder constructor.
@@ -32,5 +33,26 @@ class Bem
   public function element(string ...$elements): string
   {
     return $this->getElementName(...$elements);
+  }
+
+  public function ele($name): Bem
+  {
+    return static::block($this->element($name));
+  }
+
+  public function modify($modifier)
+  {
+    $this->_classes[] = $this->getModifier($modifier);
+    return $this;
+  }
+
+  public function asString(): string
+  {
+    return $this->_block . (!empty($this->_classes) ? " " . implode(" ", $this->_classes) : '');
+  }
+
+  public function __toString()
+  {
+    return $this->asString();
   }
 }
--- tests/BemTest.php
+++ tests/BemTest.php
@@ -25,4 +25,20 @@ class BemTest extends TestCase
   {
     self::assertEquals('btn', Bem::block('btn')->getBlockName());
   }
+
+  public function testString()
+  {
+    $btn = Bem::block('btn');
+    self::assertEquals('btn', $btn->asString());
+    $btn->modify('primary');
+    self::assertEquals('btn btn--primary', $btn->asString());
+    $btn->modify('border');
+    self::assertEquals('btn btn--primary btn--border', $btn->asString());
+    self::assertEquals('btn btn--primary btn--border', (string)$btn);
+
+    $ele = $btn->ele('icon');
+    self::assertEquals('btn__icon', $ele->asString());
+    $ele->modify('round');
+    self::assertEquals('btn__icon btn__icon--round', $ele->asString());
+  }
 }
